6dd5f3e2f69671f062a4809d5589c7b1b53fce7b
tomaszpankowski <--get-all>
2022-02-08 21:29:08 +0100

    layout done

--- src/views/home.js
+++ src/views/home.js
@@ -1,12 +1,13 @@
 import React,{Component} from "react";
-import HomeTop from "../components/home-top";
+import Container from "../../node_modules/react-bootstrap/Container";
+import IndexS1 from "../components/index-s1"
 
 class Home extends Component{
     render(){
-        return(          
-            <main className="bg-home minh-footer-adj d-flex align-items-center">     
-                <HomeTop backLink={this.props.backLink}/>
-            </main>
+        return(        
+            <Container fluid className="minh-footer-adj p-0">
+                <IndexS1/>
+            </Container>    
         );
     }
 }
--- src/views/offer.js
+++ src/views/offer.js
@@ -1,12 +1,19 @@
 import React,{Component} from "react";
-
+import Container from "../../node_modules/react-bootstrap/Container";
+import OfferS1 from "../components/offer-s1";
+import OfferS2 from "../components/offer-s2";
+import OfferS3 from "../components/offer-s3";
+import OfferS4 from "../components/offer-s4";
 
 class Offer extends Component{
     render(){
-        return(          
-            <main className="bg-offer minh-footer-adj">     
-
-            </main>
+        return(        
+            <Container fluid className="minh-footer-adj p-0">
+                <OfferS1/>
+                <OfferS2/>
+                <OfferS3/>
+                <OfferS4/>
+            </Container>    
         );
     }
 }
